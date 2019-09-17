9473ec7cf4f6e2f3dd6aa276bcf00946b153087a
andersource <andersourcer@gmail.com>
2019-09-17 18:32:13 +0300

    adding rotation controller client

---
+++ assets/device-rotation-controller/client.js
@@ -0,0 +1,23 @@
+function connect(remote_id) {
+  var peer = new Peer();
+  peer.on('open', function(id) {
+    var conn = peer.connect(remote_id);
+    conn.on('open', function() {
+      conn.on('data', function(data) {
+        console.log('Received: ', data);
+  });
+
+  var t1 = new Date();
+
+  window.addEventListener('deviceorientation', function(event) {
+    var t2 = new Date();
+      conn.send(JSON.stringify({alpha: event.alpha, beta: event.beta, gamma: event.gamma, dt_ms: t2 - t1}));
+    }, true);
+
+    });
+  });
+}
+
+window.onload = function() {
+  connect(window.location.hash.replace('#hid=', ''));
+}
