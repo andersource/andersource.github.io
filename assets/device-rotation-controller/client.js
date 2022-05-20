8b5e9aa8680bf16f91abba2261a1119764984fb9
andersource <andersourcer@gmail.com>
2022-05-20 17:23:41 +0300

    adding Lasers in Space client

--- assets/device-rotation-controller/client.js
+++ assets/device-rotation-controller/client.js
@@ -5,15 +5,32 @@ function connect(remote_id) {
     conn.on('open', function() {
       conn.on('data', function(data) {
         console.log('Received: ', data);
-  });
+      });
 
-  var t1 = new Date();
+      var t1 = new Date();
 
-  window.addEventListener('deviceorientation', function(event) {
-    var t2 = new Date();
-      conn.send(JSON.stringify({alpha: event.alpha, beta: event.beta, gamma: event.gamma, dt_ms: t2 - t1}));
-    }, true);
-
+      if (typeof( DeviceOrientationEvent ) !== "undefined"
+          && typeof( DeviceOrientationEvent.requestPermission ) === "function") {
+        DeviceOrientationEvent.requestPermission().then(response => {
+            if (response == 'granted') {
+                var t1 = new Date();
+                window.addEventListener('deviceorientation', function(event) {
+                    var t2 = new Date();
+                    conn.send(JSON.stringify({
+                        alpha: event.alpha,
+                        beta: event.beta,
+                        gamma: event.gamma,
+                        dt_ms: t2 - t1
+                    }));
+                }, true);
+            }
+        });
+      } else {
+        window.addEventListener('deviceorientation', function(event) {
+        var t2 = new Date();
+          conn.send(JSON.stringify({alpha: event.alpha, beta: event.beta, gamma: event.gamma, dt_ms: t2 - t1}));
+        }, true);
+      }
     });
   });
 }
